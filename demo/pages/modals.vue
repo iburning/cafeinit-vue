e44e7f32bb954945af0f7718226e3338e5509d8d
burning <iburning@live.cn>
2016-11-07 14:52:28 +0800

    AMModal

--- demo/pages/modals.vue
+++ demo/pages/modals.vue
@@ -1,8 +1,24 @@
 <template>
   <div id="modals">
     <div class="ci-btn-area">
+      <ci-link-button style="primary" block="block" v-on:click="isShowModal = true">Show Modal</ci-link-button>
       <ci-link-button style="primary" block="block" v-on:click="isShowAlert = true">{{isShowAlert}}</ci-link-button>
     </div>
+
+
+    <ci-modal v-bind:is-show="isShowModal" v-on:close="isShowModal = false">
+      <div>customer content</div>
+      <div slot="body">Modal Content</div>
+    </ci-modal>
+
+    <!-- <div class="am-modal am-modal-no-btn am-modal-active" tabindex="-1" style="display: block;">
+      <div class="am-modal-dialog">
+        <div class="am-modal-hd">Modal 标题</div>
+        <div class="am-modal-bd">
+          Modal 内容。本 Modal 无法通过遮罩层关闭
+        </div>
+      </div>
+    </div> -->
 
     <ci-alert v-bind:is-show="isShowAlert" v-on:close="onCloseAlert">Alert Content</ci-alert>
   </div>
@@ -13,6 +29,7 @@
 export default {
   data() {
     return {
+      isShowModal: false,
       isShowAlert: false
     }
   },
--- src/js/modal/AMModal.vue
+++ src/js/modal/AMModal.vue
@@ -1,12 +1,81 @@
-<template src="./_modal.html"></template>
+<template>
+  <div v-show="isActive" v-bind:is-close-via-dimmer="isCloseViaDimmer">
+    <div tabindex="-1"
+      v-bind:class="[
+        'am-modal',
+        'am-modal-no-btn',
+        isActive ? 'am-modal-active' : '',
+        size ? ('am-modal-' + size) : ''
+      ]"
+      v-on:click="closeViaDimmer">
+      <div class="am-modal-dialog" v-on:click="onClick">
+        <div class="am-modal-bd"><slot></slot></div>
+      </div>
+    </div>
+
+    <div v-bind:class="{
+        'am-dimmer': true,
+        'am-active': isActive
+      }"
+      transition="modal-fade"></div>
+  </div>
+</template>
 
 <script>
-import config from './_modal'
+export default {
+  props: {
+    isShow: {
+      type: Boolean,
+      default: false
+    },
 
-config.props.ns = {
-  type: String,
-  default: 'am-'
+    isCloseViaDimmer: {     // 是否通过点击遮罩层关闭模态框，默认为true
+      type: Boolean,
+      default: true
+    },
+
+    size: {
+      type: String,
+      default: ''       // lg
+    }
+  },
+
+  data() {
+    return {
+      isActive: this.isShow
+    }
+  },
+
+  watch: {
+    isShow(val) {
+      this.isActive = val
+    }
+  },
+
+  mounted() {
+    //
+  },
+
+  methods: {
+    show() {
+      this.isActive = true
+    },
+
+    close() {
+      this.isActive = false
+      this.$emit('close', 'button')
+    },
+
+    onClick(evt) {
+      evt.stopPropagation();
+    },
+
+    closeViaDimmer() {
+      if (this.isCloseViaDimmer) {
+        this.isActive = false
+        this.$emit('close', 'dimmer')
+      }
+    }
+  }
 }
-
-export default config
 </script>
