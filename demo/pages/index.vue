9d18e2be96809964835482d80f9b7acfd24a3d10
burning <iburning@live.cn>
2016-11-27 23:48:19 +0800

    index

--- demo/pages/index.vue
+++ demo/pages/index.vue
@@ -144,6 +144,11 @@
         <span class="sub-title">固定元素</span>
       </ci-list-cell>
 
+      <ci-list-cell accessory="link" to="/to-top">
+        <strong class="title">To Top</strong>
+        <span class="sub-title">返回顶部</span>
+      </ci-list-cell>
+
       <ci-list-cell accessory="link" to="/page-demo">
         <strong class="title">Page Demo</strong>
         <span class="sub-title">页面实例</span>
--- demo/pages/rate.vue
+++ demo/pages/rate.vue
@@ -9,13 +9,9 @@
 </template>
 
 <script>
-import CIRate from '../components/CIRate.vue'
-
 export default {
   name: 'page-rate',
-  components: {
-    'ci-rate': CIRate
-  },
+
   data() {
     return {
       rate: 3.75
