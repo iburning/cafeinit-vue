b56cd3bc0b763f6e43e6ae3b6f4d277b71fa4b9d
burning <iburning@live.cn>
2017-01-16 11:18:53 +0800

    bug fixed 在微信中不能及时处理isListenScrollToEnd

--- src/js/utils/CIScrollHelper.js
+++ src/js/utils/CIScrollHelper.js
@@ -60,6 +60,7 @@ prototype._bindEvent = function () {
         if (typeof this.willScrollToEndHandler === 'function') {
           this.willScrollToEndHandler()
         }
+        this.isListenScrollToEnd = true
       }
     }
     else {
